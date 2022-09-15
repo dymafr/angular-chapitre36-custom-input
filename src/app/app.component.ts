c563e29bc625b8910afdc3fa3ff382cecba64f78
dymafr <contact@dyma.fr>
2022-09-15 19:02:35 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular-chapitre36-custom-input';
+}
